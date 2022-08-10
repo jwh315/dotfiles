3eb716c451f8b2668adc890e4e0a4089d1a6f0ee
Jacob Holmes <Jacob.Holmes@coxautoinc.com>
2022-08-10 07:56:41 -0700

    fix symlinks

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # symlink configuration files
 mkdir -p ~/.config
 
-ln -s "$DOTFILES_DIR/nvim" ~/.config/nvim
-ln -s "$DOTFILES_DIR/.gitconfig" ~/.gitconfig
+ln -sf "$DOTFILES_DIR/nvim" ~/.config
+ln -sf "$DOTFILES_DIR/.gitconfig" ~/.gitconfig
 
 # Install Homebrew
 . "$DOTFILES_DIR/install/brew.sh"
