86be610488d913278712516c2b402b6895b8b96e
Jacob Holmes <Jacob.Holmes@coxautoinc.com>
2024-05-15 12:50:28 -0700

    Add oil nvim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -259,7 +259,28 @@ require('lazy').setup {
   -- plugin to install our preferred theme
   { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = {} },
 
-  { 'pocco81/auto-save.nvim' },
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        columns = { 'icon' },
+        keymaps = {
+          ['<C-h>'] = false,
+          ['<M-h>'] = 'actions.select_split',
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+      -- Open parent directory in floating window
+      vim.keymap.set('n', '<space>-', require('oil').toggle_float)
+    end,
+  },
 
   { 'phaazon/hop.nvim', opts = {} },
 
