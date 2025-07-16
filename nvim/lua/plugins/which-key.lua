6bf288cfda2404566df08e85fd50e29b05a9622a
Jacob Holmes <Jacob.Holmes@coxautoinc.com>
2025-07-15 19:42:31 -0700

    feat: add spellcheck suggestion in which-key

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -10,6 +10,23 @@ return {
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+      ['<leader>z'] = { name = '[Z] Spell', _ = 'which_key_ignore' },
     }
+
+    -- Register spell check mappings under leader
+    require('which-key').register({
+      ['<leader>z='] = { 'z=', 'Spell suggestions' },
+      ['<leader>zg'] = { 'zg', 'Add word to dictionary' },
+      ['<leader>zw'] = { 'zw', 'Mark word as wrong' },
+      ['<leader>zug'] = { 'zug', 'Undo add word' },
+      ['<leader>zuw'] = { 'zuw', 'Undo mark word' },
+      ['<leader>zn'] = { ']s', 'Next misspelled word' },
+      ['<leader>zp'] = { '[s', 'Previous misspelled word' },
+    }, {
+      mode = 'n',
+      buffer = nil,
+      silent = true,
+      noremap = true,
+    })
   end,
 }
