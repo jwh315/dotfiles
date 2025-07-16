d8dd4e10dd42a6933306a2f94378259f7a2e608b
Jacob Holmes <Jacob.Holmes@coxautoinc.com>
2025-07-15 20:04:26 -0700

    feat: add which-key rule for gitsigns

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,6 +1,6 @@
 -- Displays available keybindings in popup when you start typing a command
 -- Helps discover and remember key mappings by showing them in a helpful popup
--- Customization: Includes spell check mappings under <leader>z for enhanced spell checking
+-- Customization: Includes spell check and git mappings for enhanced discoverability
 return {
   'folke/which-key.nvim',
   event = 'VimEnter',
@@ -14,6 +14,7 @@ return {
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>z'] = { name = '[Z] Spell', _ = 'which_key_ignore' },
+      ['<leader>g'] = { name = '[G] Git', _ = 'which_key_ignore' },
     }
 
     -- Register spell check mappings under leader
@@ -27,6 +28,39 @@ return {
       ['<leader>zp'] = { '[s', 'Previous misspelled word' },
     }, {
       mode = 'n',
+      buffer = nil,
+      silent = true,
+      noremap = true,
+    })
+
+    -- Register git mappings using gitsigns functions
+    require('which-key').register({
+      ['<leader>gb'] = { function() require('gitsigns').blame_line() end, 'Git blame line' },
+      ['<leader>gB'] = { function() require('gitsigns').blame_line { full = true } end, 'Git blame line (full)' },
+      ['<leader>gd'] = { function() require('gitsigns').diffthis() end, 'Git diff this' },
+      ['<leader>gD'] = { function() require('gitsigns').diffthis('~') end, 'Git diff this (cached)' },
+      ['<leader>gp'] = { function() require('gitsigns').preview_hunk() end, 'Git preview hunk' },
+      ['<leader>gr'] = { function() require('gitsigns').reset_hunk() end, 'Git reset hunk' },
+      ['<leader>gR'] = { function() require('gitsigns').reset_buffer() end, 'Git reset buffer' },
+      ['<leader>gs'] = { function() require('gitsigns').stage_hunk() end, 'Git stage hunk' },
+      ['<leader>gS'] = { function() require('gitsigns').stage_buffer() end, 'Git stage buffer' },
+      ['<leader>gu'] = { function() require('gitsigns').undo_stage_hunk() end, 'Git undo stage hunk' },
+      ['<leader>gn'] = { function() require('gitsigns').next_hunk() end, 'Git next hunk' },
+      ['<leader>gN'] = { function() require('gitsigns').prev_hunk() end, 'Git previous hunk' },
+      ['<leader>gt'] = { function() require('gitsigns').toggle_current_line_blame() end, 'Git toggle blame' },
+    }, {
+      mode = 'n',
+      buffer = nil,
+      silent = true,
+      noremap = true,
+    })
+
+    -- Register git mappings for visual mode
+    require('which-key').register({
+      ['<leader>gs'] = { function() require('gitsigns').stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, 'Git stage hunk' },
+      ['<leader>gr'] = { function() require('gitsigns').reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, 'Git reset hunk' },
+    }, {
+      mode = 'v',
       buffer = nil,
       silent = true,
       noremap = true,
